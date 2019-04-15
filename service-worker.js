8c22d43da9343ad8bbb47a08858bbf1df3ebd3d5
Alan Uecker <fleddermaus2@gmail.com>
2019-04-15 20:32:11 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5281c400d9165de265ba930b89f7863.js"
+  "/precache-manifest.4442bfa8616d883d1111ab6ee09dcedb.js"
 );
 
 workbox.clientsClaim();
