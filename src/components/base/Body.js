1d4a8bf51e23a4639ba60813840a4fc9b336398c
Alan Uecker <alan.uecker@img.ly>
2022-12-05 20:04:17 +0100

    removed old files

--- src/components/base/Body.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const Body = styled.p`
-  font-size: 0.875rem;
-  padding: 1rem;
-  margin: 0;
-`;
-
-export default Body;
--- src/components/base/H2.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const H2 = styled.h2`
-  font-weight: normal;
-  letter-spacing: 0.125rem;
-  font-size: 1rem;
-`;
-
-export default H2;
