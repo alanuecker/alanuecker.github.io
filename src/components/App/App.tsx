256852883829428c01d91d3042d9679d265c5735
Alan Uecker <alan.uecker@img.ly>
2023-02-26 19:57:49 +0100

    programm frame

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,11 +9,15 @@ import classes from './style.module.scss';
 export const App: React.FC = () => {
   return (
     <div className={classes.root}>
-      <section className={classes.hero}>
-        <Name />
-        <Introduction />
-      </section>
-      <Footer />
+      <div className={classes.window}>
+        <div className={classes.background} />
+        <div className={classes['frame-accent']} />
+        <div className={classes['frame-light']} />
+        <div className={classes['frame-dark']} />
+        <div className={classes['outer-frame-accent']} />
+        <div className={classes['outer-frame-light']} />
+        <div className={classes['outer-frame-dark']} />
+      </div>
     </div>
   );
 };
