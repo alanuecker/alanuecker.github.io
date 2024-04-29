51e71566eeed601449e74528fab93599ac59fdc0
Alan Uecker <uecker.alan@gmail.com>
2024-04-29 11:02:49 +0200

    removed old content

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
 import { Footer } from '../Footer';
-import { Introduction } from '../Introduction';
-import { Name } from '../Name';
-import { Window } from '../Window';
 
 import classes from './style.module.scss';
 
 export const App: React.FC = () => {
   return (
     <div className={classes.root}>
-      <Window>
-        <Name />
-        <Introduction />
-        <Footer />
-      </Window>
+
     </div>
   );
 };
