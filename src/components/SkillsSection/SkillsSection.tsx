8f8d51e20aaf09fafac5f47a47ddf143df09a00c
Alan Uecker <uecker.alan@gmail.com>
2024-04-30 17:51:44 +0200

    added skills section

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -5,29 +5,31 @@ import { Section } from '../Section';
 import classes from './style.module.scss';
 
 const SKILLS = [
-  'TYPESCRIPT',
-  'HTML',
-  'SASS',
-  'REACT',
-  'CSS',
-  'WEBSITES',
-  'JAVASCRIPT',
-  'AGILE',
-  'FIGMA',
-  'NODE.JS',
-  'CROSS-PLATFORM',
-  'UI/UX Design',
-  'UX Research',
+  { name: 'TYPESCRIPT', weight: 10 },
+  { name: 'REACT', weight: 10 },
+  { name: 'HTML', weight: 8 },
+  { name: 'SASS', weight: 6 },
+  { name: 'CSS', weight: 8 },
+  { name: 'WEBSITES', weight: 10 },
+  { name: 'JAVASCRIPT', weight: 8 },
+  { name: 'AGILE', weight: 2 },
+  { name: 'FIGMA', weight: 4 },
+  { name: 'NODE.JS', weight: 4 },
+  { name: 'CROSS-PLATFORM', weight: 2 },
+  { name: 'UI/UX Design', weight: 4 },
+  { name: 'UX Research', weight: 4 },
 ];
 
 export function SkillsSection(): React.JSX.Element {
   return (
     <Section className={classes.root} id="skills" title="Skills">
-      <div className={classes.container}>
-        {SKILLS.map(skill => (
-          <div key={skill}>{skill}</div>
+      <ul className={classes.container}>
+        {SKILLS.sort((a, b) => 0.5 - Math.random()).map(({ name, weight }) => (
+          <li key={name} className={classes.skill} data-weigth={weight}>
+            {name}
+          </li>
         ))}
-      </div>
+      </ul>
     </Section>
   );
 }
