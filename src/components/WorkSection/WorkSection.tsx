78a8c3aac5961d1cd2adcd3eb5b878ba7824154e
Alan Uecker <uecker.alan@gmail.com>
2024-04-30 17:51:53 +0200

    added work section

--- src/components/WorkSection/WorkSection.tsx
+++ src/components/WorkSection/WorkSection.tsx
@@ -7,18 +7,57 @@ import classes from './style.module.scss';
 export function WorkSection(): React.JSX.Element {
   return (
     <Section className={classes.root} id="work" title="Work & Education">
-      <div>
-        03/2024 - 07/2024 UI/UX Bootcamp Learning the basics of UI/UX Research
-        and Design
+      <div className={classes.item}>
+        <div className={classes.item__date_container}>
+          <span className={classes.item__date}>
+            03/2024 -<br />
+            07/2024
+          </span>
+        </div>
+        <div className={classes.item__container}>
+          <span className={classes.item__title}>UI/UX Bootcamp - Student</span>
+          Learning the basics of UI/UX Research and Design
+        </div>
       </div>
-      <div>
-        IMG.LY 11/2019 - 01/2024 I develop and maintain the documentation
-        experience for all SDKs. This included Markdown-based features to
-        improve the writing process and the front-end build in React.Previously,
-        I was heavily involved in developing and maintaining the latest version
-        of PE.SDK Web and all its public repositories and examples.
+      <div className={classes.item}>
+        <div className={classes.item__date_container}>
+          <span className={classes.item__date}>
+            11/2019 -<br />
+            01/2024
+          </span>
+        </div>
+        <div className={classes.item__container}>
+          <span className={classes.item__title}>
+            IMG.LY GmbH - Software Engineer
+          </span>
+          <span>
+            I partenered with a Senior Developer to create a web-based image
+            editor in React and TypeScript. This also included customer support
+            and maintaining a npm library and it's integration examples.
+          </span>
+          <span>
+            Afterwards I led the development of a documentation platform using
+            React, Typescript, and Gatsby, that was specifically designed to
+            meet the needs of internal developers.
+          </span>
+        </div>
       </div>
-      <div>11/2019 - 01/2024 RISE Technologies Frontend Development</div>
+      <div className={classes.item}>
+        <div className={classes.item__date_container}>
+          <span className={classes.item__date}>
+            06/2017 -<br />
+            08/2019
+          </span>
+        </div>
+        <div className={classes.item__container}>
+          <span className={classes.item__title}>
+            rise technologies GmbH - Software Engineer
+          </span>
+          I worked in a team of developers to create a communication-focused
+          SaaS platform in React and JavaScript. I also collaborated with a UI
+          designer to create a component library.
+        </div>
+      </div>
     </Section>
   );
 }
