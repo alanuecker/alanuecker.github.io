50e2b0334189c3ee2993d0cef4776965e4dbf92a
Alan Uecker <uecker.alan@gmail.com>
2024-05-03 11:38:21 +0200

    added project section

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -4,14 +4,20 @@ import { Section } from '../Section';
 
 import classes from './style.module.scss';
 
-interface Props {}
-
-export function ProjectsSection({}: Props): React.JSX.Element {
+export function ProjectsSection(): React.JSX.Element {
   return (
-    <Section
-      className={classes.root}
-      id="projects"
-      title="Selected Work"
-    ></Section>
+    <Section className={classes.root} id="projects" title="Selected Work">
+      <div className={classes.item}>
+        <div className={classes.item__container}>
+          <span className={classes.item__title}>Project 1</span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur. Volutpat justo ultrices
+            tempor tristique ac faucibus at quis tincidunt. Justo malesuada
+            euismod praesent fames erat. Scelerisque aliquet ullamcorper aliquam
+            quam diam sed. Sapien commodo quam nisi habitant aenean.
+          </span>
+        </div>
+      </div>
+    </Section>
   );
 }
