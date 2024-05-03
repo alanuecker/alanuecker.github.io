af166d5f4b98c059f069d22d62634128ecb5a6d4
Alan Uecker <uecker.alan@gmail.com>
2024-05-03 11:37:37 +0200

    added skills section

--- src/components/SkillsSection/SkillsSection.tsx
+++ src/components/SkillsSection/SkillsSection.tsx
@@ -2,34 +2,69 @@ import React from 'react';
 
 import { Section } from '../Section';
 
-import classes from './style.module.scss';
+import codingImageUrl from '../../img/coding.png';
+import responsiveImageUrl from '../../img/responsive-design.png';
+import digitalImageUrl from '../../img/digital.png';
+import controlPanelImageUrl from '../../img/control-panel.png';
 
-const SKILLS = [
-  { name: 'TYPESCRIPT', weight: 10 },
-  { name: 'REACT', weight: 10 },
-  { name: 'HTML', weight: 8 },
-  { name: 'SASS', weight: 6 },
-  { name: 'CSS', weight: 8 },
-  { name: 'WEBSITES', weight: 10 },
-  { name: 'JAVASCRIPT', weight: 8 },
-  { name: 'AGILE', weight: 2 },
-  { name: 'FIGMA', weight: 4 },
-  { name: 'NODE.JS', weight: 4 },
-  { name: 'CROSS-PLATFORM', weight: 2 },
-  { name: 'UI/UX Design', weight: 4 },
-  { name: 'UX Research', weight: 4 },
-];
+import classes from './style.module.scss';
 
 export function SkillsSection(): React.JSX.Element {
   return (
-    <Section className={classes.root} id="skills" title="Skills">
-      <ul className={classes.container}>
-        {SKILLS.sort((a, b) => 0.5 - Math.random()).map(({ name, weight }) => (
-          <li key={name} className={classes.skill} data-weigth={weight}>
-            {name}
-          </li>
-        ))}
-      </ul>
+    <Section
+      className={classes.root}
+      id="skills"
+      title="Skills"
+      showBorder={false}
+    >
+      <div className={classes.container}>
+        <div className={classes.item}>
+          <img
+            src={codingImageUrl}
+            alt="Depiction of a web development view"
+            className={classes.item__image}
+          ></img>
+          <span className={classes.item__title}>Web Development</span>
+          <span className={classes.item__body}>
+            I'm an expert in HTML5, CSS3, JavaScript (ES6+), TypeScript, React,
+            and Node.js
+          </span>
+        </div>
+        <div className={classes.item}>
+          <img
+            src={responsiveImageUrl}
+            alt="Depiction of a design displayed on web and mobile devices"
+            className={classes.item__image}
+          ></img>
+          <span className={classes.item__title}>Responsive Design</span>
+          <span className={classes.item__body}>
+            Creating visually appealing and functional websites across devices
+          </span>
+        </div>
+        <div className={classes.item}>
+          <img
+            src={controlPanelImageUrl}
+            alt="Depiction of a digital control panel"
+            className={classes.item__image}
+          ></img>
+          <span className={classes.item__title}>Tools</span>
+          <span className={classes.item__body}>
+            I use tools like Git, GitHub Actions, Jest, Playwright, ESLint,
+            Prettier, and Vite daily
+          </span>
+        </div>
+        <div className={classes.item}>
+          <img
+            src={digitalImageUrl}
+            alt="Depiction of digital design process"
+            className={classes.item__image}
+          ></img>
+          <span className={classes.item__title}>UI/UX Design</span>
+          <span className={classes.item__body}>
+            Research, wireframing, prototyping, user flow analysis
+          </span>
+        </div>
+      </div>
     </Section>
   );
 }
