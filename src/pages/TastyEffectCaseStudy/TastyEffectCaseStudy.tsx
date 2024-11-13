1ea73198ef732a45cc08aa9dd7a2cc3f85a38ee0
Alan Uecker <uecker.alan@gmail.com>
2024-11-13 18:07:20 +0100

    ran lint:ts --fix

--- src/pages/TastyEffectCaseStudy/TastyEffectCaseStudy.tsx
+++ src/pages/TastyEffectCaseStudy/TastyEffectCaseStudy.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './style.module.scss';
+// import classes from './style.module.scss';
 
 export function TastyEffectCaseStudy(): React.JSX.Element {
   return (
